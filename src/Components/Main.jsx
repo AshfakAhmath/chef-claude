4326d4dee7c31d785a0ec7d55e3ad2408f399338
Ashfak Ahmath <ashfakahmath57@gmail.com>
2026-08-14 19:12:02 +0530

    feat: implement ingredient addition functionality and improve form handling

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,13 +1,31 @@
-function Main(){
+function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  const listIngrediants = ingredients.map((E) => {
+    return <li>{E}</li>;
+  });
 
-    return(
-        <main>
-            <form className="add-ingredient-form">
-                <input type="text" aria-label="Add ingredient" placeholder="e.g. oregano"/>
-                <button>Add ingredient</button>
-            </form>
-        </main>
-    )
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    ingredients.push(newIngredient);
+    console.log(ingredients);
+  }
+  return (
+    <main>
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
+        <input
+          type="text"
+          aria-label="Add ingredient"
+          placeholder="e.g. oregano"
+          name="ingredient"
+        />
+        <button>Add ingredient</button>
+      </form>
+
+      <ul>{listIngrediants}</ul>
+    </main>
+  );
 }
 
-export default Main
+export default Main;
