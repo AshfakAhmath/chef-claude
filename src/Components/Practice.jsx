c3a118c9601c330ce10122708129f064b7580c2f
Ashfak Ahmath <ashfakahmath57@gmail.com>
2026-08-18 20:04:30 +0530

    fix: update state handling in Practice component to use functional updates

--- src/Components/Practice.jsx
+++ src/Components/Practice.jsx
@@ -4,11 +4,11 @@ function Practice(){
     const [count, setCount] = React.useState(0);
 
     function handleMinus(){
-        setCount(count - 1);
+        setCount(prevCount => prevCount - 1);
     }
 
     function handlePlus(){
-        setCount(count + 1);
+        setCount(prevCount => prevCount + 1);
     }
 
    return (
