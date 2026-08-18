405708663e8c3fda5a45a3978ceac2123dde2dd0
Ashfak Ahmath <ashfakahmath57@gmail.com>
2026-08-18 20:34:03 +0530

    feat: enhance Practice component with state management and styling for user interaction

--- src/Components/Practice.jsx
+++ src/Components/Practice.jsx
@@ -1,26 +1,53 @@
 import React from "react";
 
-function Practice(){
-    const [count, setCount] = React.useState(0);
+function Practice() {
+  const [count, setCount] = React.useState(0);
 
-    function handleMinus(){
-        setCount(prevCount => prevCount - 1);
-    }
+  const [isGoingOut, setIsGoingOut] = React.useState("Yes");
+console.log(isGoingOut);
+  function handleClick() {
+    setIsGoingOut((prevValue) => (prevValue ? "No" : "Yes"));
+  }
 
-    function handlePlus(){
-        setCount(prevCount => prevCount + 1);
-    }
+  function handleMinus() {
+    setCount((prevCount) => prevCount - 1);
+  }
 
-   return (
-        <div className="container">
-            <h1>How many times will Bob say "state" in this section?</h1>
-            <div className="counter">
-                <button onClick={handleMinus} className="minus" aria-label="Decrease count">–</button>
-                <h2 className="count">{count}</h2>
-                <button onClick={handlePlus} className="plus" aria-label="Increase count">+</button>
-            </div>
+  function handlePlus() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h1>How many times will Bob say "state" in this section?</h1>
+        <div className="counter">
+          <button
+            onClick={handleMinus}
+            className="minus"
+            aria-label="Decrease count"
+          >
+            –
+          </button>
+          <h2 className="count">{count}</h2>
+          <button
+            onClick={handlePlus}
+            className="plus"
+            aria-label="Increase count"
+          >
+            +
+          </button>
         </div>
-    )
+      </div>
+
+      <main>
+        <h1 className="title">Do I feel like going out tonight?</h1>
+        <button onClick={handleClick} className="value">
+          {isGoingOut}
+        </button>
+      </main>
+    </>
+  );
 }
 
-export default Practice
+export default Practice;
