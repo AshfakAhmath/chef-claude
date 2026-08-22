5b11f8465242497548ef04aac3e0a982bc45b1c1
Ashfak Ahmath <ashfakahmath57@gmail.com>
2026-08-22 23:58:11 +0530

    feat: add favorite things functionality and update styles in Practice component

--- src/Components/Practice.jsx
+++ src/Components/Practice.jsx
@@ -17,6 +17,32 @@ function Practice() {
     setCount((prevCount) => prevCount + 1);
   }
 
+  const [myFavoriteThings, setMyFavoriteThings] = React.useState([]);
+
+  const allFavoriteThings = [
+    "💦🌹",
+    "😺",
+    "💡🫖",
+    "🔥🧤",
+    "🟤🎁",
+    "🐴",
+    "🍎🥧",
+    "🚪🔔",
+    "🛷🔔",
+    "🥩🍝",
+  ];
+  const thingsElements = myFavoriteThings.map((thing) => (
+    <p key={thing}>{thing}</p>
+  ));
+
+  function addFavoriteThing() {
+    setMyFavoriteThings((prevValue) => [
+      ...prevValue,
+      allFavoriteThings[prevValue.length],
+    ]);
+    console.log(myFavoriteThings);
+  }
+
   return (
     <>
       <div className="container">
@@ -42,10 +68,19 @@ function Practice() {
 
       <main>
         <h1 className="title">Do I feel like going out tonight?</h1>
-        <button onClick={handleClick} className="value">
+        <button
+          onClick={handleClick}
+          aria-label={`Current answer is ${isGoingOut ? "Yes" : "No"}. Click to change it.`}
+          className="value"
+        >
           {isGoingOut ? "Yes" : "No"}
         </button>
       </main>
+
+      <div className="array">
+        <button onClick={addFavoriteThing}>Add item</button>
+        <section aria-live="polite">{thingsElements}</section>
+      </div>
     </>
   );
 }
